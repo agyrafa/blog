018248a8ac4a721b0a5c2116a0585bf4fdf4d187
FranCOder <maximfranko96@gmail.com>
2016-04-23 12:27:52 +0600

    Input Validation

--- resources/views/includes/error_messages.blade.php
+++ resources/views/includes/error_messages.blade.php
@@ -1,5 +1,8 @@
 @foreach($errors->all() as $error)
     <div class="alert alert-danger errors">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" title="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <ul>
             <li>{{ $error }}</li>
         </ul>
